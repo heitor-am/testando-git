6deac81130096f5015b716b3c8d288b4d113e6df
Heitor Moura <heitorandrade2001@gmail.com>
2021-10-05 21:27:00 -0300

    modificações

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-const soma = (a, b) => a + b
+const keys = {
+  ArrowUp() {
+    return "Para cima"
+  },
+  'ArrowDown': () => "Para baixo",
+  'ArrowRight': () => "Para direita",
+  "ArrowLeft": () => "Para esquerda"
+}
 
-console.log(soma(2, 1))
+window.addEventListener('keydown', (e) => {
+  console.log(keys[e.key])
+})
